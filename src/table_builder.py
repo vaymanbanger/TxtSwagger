50eb3d65338792a42ff0b7d19712f35bd3092eb4
vaymanbanger <vityak2019@mail.ru>
2025-10-11 08:38:04 +0300

    added visual separation of tables

--- src/table_builder.py
+++ src/table_builder.py
@@ -18,15 +18,31 @@ def convert_json_to_table(data: list[dict]) -> str:
             json_keys.update(item.keys())
     
     json_keys = sorted(json_keys, key=len)
-            
+    
     def format_value(value):
         if isinstance(value,(str, int, bool, float)):
             return str(value)
         return type(value).__name__
     
-    title = ["\t".join(json_keys)]
+    rows = [list(json_keys)]
     for item in data:
         row = [format_value(item.get(key)) for key in json_keys]
-        title.append("\t".join(row))
+        rows.append(row)
     
-    return "\n".join(title)
+    column_width = []
+    for index in range(len(json_keys)):
+        max_width = max(len(row[index]) for row in rows)
+        column_width.append(max_width)
+    
+    def format_row(row):
+        return "| " + " | ".join(f"{value:<{column_width[idx]}}"
+                                 for idx, value in enumerate(row)) + " |"
+    
+    separator = "|".join("-" * width for width in column_width) + "|"
+
+    result = [separator, format_row(rows[0]), separator]
+    for row in rows[1:]:
+        result.append(format_row(row))
+    result.append(separator)
+    
+    return "\n".join(result)
